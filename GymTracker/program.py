0ea8fb2db4556a8a7c9021d1eea9118d454af9aa
Raz Damaschin <razdjp@gmail.com>
2017-12-05 07:16:45 -0600

    R1D2 - Day 2

--- GymTracker/program.py
+++ GymTracker/program.py
@@ -1,35 +1,48 @@
 import os
+import platform
+
+def detect_os():
+	if platform.system() == 'Windows':
+		return 'cls'
+	else:
+		return 'clear'
+
+clear_command = detect_os()
+filename = 'tracker.txt'
 
 def main():
 	print_header()
 	main_logic()
 
 def print_header():
-	os.system('clear')
+	os.system(clear_command)
 	print('####################')
 	print('# Gym Tracker v1.0 #')
 	print('####################')
 
 def main_logic():
-	entries = []
-
+	entries = load()
 	opt = None
 
 	while opt != 'x':
 		print("Chose one of the following options:")
-		opt = input('[G]roup Training, [S]elf Training, [T]otal Points, E[x]it: ')
+		opt = input('[G]roup Training * [A]lone Training * [T]otal Points * [S]ave * E[x]it: ')
 		opt = opt.lower()
 		if opt == 'g':
 			print_header()
 			print("Group Training added. Good Job!")
 			add_event(entries, 6)
-		elif opt == 's':
+		elif opt == 'a':
 			print_header()
 			print("Self Training added. Way to go!")
 			add_event(entries, 3)
 		elif opt == 't':
 			print_header()
 			total_points(entries)
+		elif opt == 's':
+			print_header()
+			print("Data saved.")
+			save(entries)
 		elif opt != 'x':
 			print_header()
 			print("Option does not exist. Please try again!")
@@ -42,6 +55,21 @@ def total_points(data):
 	for entry in data:
 		total += entry
 	print("You have {} entries so far, with a total of {} points. You are doing great!".format(len(data), total))
+
+def load():
+	content = []
+	try:
+		with open(filename, 'r') as filein:
+			content = filein.readlines()
+		content = [int(x.strip()) for x in content]
+	except IOError:
+		pass
+	return content
+
+def save(data):
+	with open(filename, 'w') as fileout:
+		for points in data:
+			fileout.write(str(points) + '\n')
 
 if __name__ == '__main__':
 	main()
